bca4a169ce212a77c59dd8c70f1acb0b879ed331
Jason Yegge <Lansatac@gmail.com>
2021-06-22 14:20:45 -0700

    Adding EnumeratorExtensions and AsStrand

---
+++ Packages/com.unity.strands/Assets/Runtime/EnumeratorExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+
+namespace Strands
+{
+    public static class EnumeratorExtensions
+    {
+        /// <summary>
+        /// Promotes an IEnumerator interface to a Strand.
+        /// </summary>
+        /// <param name="enumerator">IEnumerator to convert to a strand.</param>
+        /// <returns>enumerator as a strand.</returns>
+        public static Strand AsStrand(this IEnumerator enumerator)
+        {
+            if (enumerator is Strand strand)
+                return strand;
+            return new EnumeratorStrand(enumerator);
+        }
+    }
+}
