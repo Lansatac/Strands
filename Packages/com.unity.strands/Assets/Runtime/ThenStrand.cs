28bcb4f67c8b33e575b08e32f09e7017870ef64f
Jason Yegge <Lansatac@gmail.com>
2021-06-22 14:35:29 -0700

    Adding ThenStrand and Then

---
+++ Packages/com.unity.strands/Assets/Runtime/ThenStrand.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+
+namespace Strands
+{
+    public class ThenStrand<TFirst, TSecond> : Strand
+        where TFirst : IEnumerator
+        where TSecond : IEnumerator
+    {
+        private readonly TFirst _first;
+        private readonly Func<TFirst, TSecond> _secondGenerator;
+
+        public ThenStrand(TFirst first, Func<TFirst, TSecond> secondGenerator)
+        {
+            _first = first;
+            _secondGenerator = secondGenerator;
+        }
+
+        protected override IEnumerator Execute()
+        {
+            while (_first.MoveNext())
+            {
+                yield return _first.Current;
+            }
+
+            var second = _secondGenerator(_first);
+            while (second.MoveNext())
+            {
+                yield return second.Current;
+            }
+        }
+    }
+}
