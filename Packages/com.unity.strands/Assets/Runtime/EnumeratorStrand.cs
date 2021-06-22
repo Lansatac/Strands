bf1e4aa08fbe89712e736ead394df0eb4701de32
Jason Yegge <Lansatac@gmail.com>
2021-06-22 14:20:45 -0700

    Adding EnumeratorExtensions and AsStrand

---
+++ Packages/com.unity.strands/Assets/Runtime/EnumeratorStrand.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+
+namespace Strands
+{
+    public class EnumeratorStrand : Strand
+    {
+        private readonly IEnumerator _enumerator;
+
+        public EnumeratorStrand(IEnumerator enumerator)
+        {
+            _enumerator = enumerator;
+        }
+
+        protected override IEnumerator Execute()
+        {
+            while (_enumerator.MoveNext())
+            {
+                yield return _enumerator.Current;
+            }
+        }
+    }
+}
